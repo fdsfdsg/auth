3f71d8895bac4662196cdc4b20ce3c647dc54a19
park sangbeom <smctkdqja@gmail.com>
2020-10-16 21:37:59 +0900

    bcrypt를 이용한 패스워드 암호화

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
 
-const mongoose = require('mongoose')
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
 mongoose
-  .connect(
-    'mongodb+srv://tkdqja4164:t1k2d301@fullstack.8t9cn.mongodb.net/test?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("몽고디비 연결됨"))
   .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("ㅎㅇ");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 할때 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어줌
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 10,
+    maxlength: 50,
   },
   email: {
     type: String,
@@ -12,7 +14,11 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 20,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
   },
   role: {
     type: Number, //1이면 관리자, 0이면 유저
@@ -26,6 +32,23 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function(next) {
+  //비밀번호를 암호화 시킨다.
+  let user = this;
+  console.log(user);
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+}); //mongoose 메소드 => 유저 정보 저장하기 전에 콜백
 
 const User = mongoose.model("User", userSchema);
 
