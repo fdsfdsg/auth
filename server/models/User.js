285512db99b9beffe21c0c2500a5d9d94d0adf54
park sangbeom <smctkdqja@gmail.com>
2020-10-17 16:01:24 +0900

    server 로그인 로그아웃 구현

---
+++ server/models/User.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //spacebar 없애주려고
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number, //1이면 관리자, 0이면 유저
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  //화살표함수로 쓰면 제대로 안감 , 찾아본 결과 몽구스 공식 문서에는 화살표함수는 this를 바인딩 하지 않아서 User의 다큐멘트를 가질수 없어서 그렇다고한다.
+  //비밀번호를 암호화 시킨다.
+  let user = this;
+  console.log(user);
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+}); //mongoose 메소드 => 유저 정보 저장하기 전에 콜백
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  //jsonwebtoken을 이용한 token 생성
+
+  var token = jwt.sign(user._id.toHexString(), "secretToken"); // 두번쨰 인자는 암호화 복호화할떄 씀
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  //토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
